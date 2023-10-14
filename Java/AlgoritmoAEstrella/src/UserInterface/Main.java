c5fba7ff8d308850fae44c11632fcdd39e87c0d6
Martin-Roberto-lecuona <robertlecuona@gmail.com>
2023-10-14 15:22:14 -0300

    Anda bien y esta limpio el codigo
    falta alguno prueba exsaustiva pero anda casi perfecto con los casos medio raros

--- Java/AlgoritmoAEstrella/src/UserInterface/Main.java
+++ Java/AlgoritmoAEstrella/src/UserInterface/Main.java
@@ -13,14 +13,11 @@ public class Main {
 		Mapa m = new Mapa(mapaMatrix);
 		m.printMapa();
 
-		Casilla orig = new Casilla(0, 0);
-		Casilla dest = new Casilla(11,11);
+		Casilla orig = new Casilla(10, 0);
+		Casilla dest = new Casilla(5,11);
 		ArrayList<Casilla> camino = m.camino(orig, dest);
 		System.out.println("Camino: ");
 		m.printCamino(camino);
-		for (Casilla x : camino) {
-			System.out.println(x + " padre " + x.getpadre() + " -> ");
-		}
 	}
 
 }
--- Java/AlgoritmoAEstrella/src/entitis/Mapa.java
+++ Java/AlgoritmoAEstrella/src/entitis/Mapa.java
@@ -7,9 +7,12 @@ import java.util.NoSuchElementException;
 
 public class Mapa {
 	private Double[][] terreno;
+	int x, y;
 
 	public Mapa(Double[][] t) {
 		terreno = t;
+		x = terreno.length;
+		y = terreno[0].length;
 	}
 
 	public boolean esObstaculo(Casilla c) {
@@ -26,13 +29,13 @@ public class Mapa {
 
 	public void printMapa() {
 		System.out.print("   ");
-		for (int i = 0; i < terreno[0].length; i++) {
+		for (int i = 0; i < this.y; i++) {
 			System.out.print(" " + i + " ");
 		}
 		System.out.println();
-		for (int i = 0; i < terreno.length; i++) {
+		for (int i = 0; i < this.x; i++) {
 			System.out.print(" " + i + " ");
-			for (int j = 0; j < terreno[0].length; j++) {
+			for (int j = 0; j < this.y; j++) {
 				if (!esObstaculo(i, j))
 					System.out.print("|○|");
 				else
@@ -43,71 +46,68 @@ public class Mapa {
 	}
 
 	public ArrayList<Casilla> camino(Casilla origen, Casilla destino) {
-		//
-		// check si existe o es obstaculo si destino u origen
-		//
+		if (!casillaValida(origen)) {
+			System.out.println("la casilla de origen no es valida");
+			return null;
+		}
+		if (!casillaValida(destino)) {
+			System.out.println("la casilla de destino no es valida");
+			return null;
+		}
 		ArrayList<Casilla> cerrado = new ArrayList<Casilla>();
 		origen.setH(getDistanciaEntreCasilas(origen, destino));
 		Casilla act = origen;
 		cerrado.add(act);
 		ArrayList<Casilla> sinSalida = new ArrayList<Casilla>();
-		ArrayList<Casilla> todosVecinos = new ArrayList<Casilla>();
-		ArrayList<Casilla> vecinos = getVecinos(act, cerrado, destino,sinSalida);
-		ArrayList<Casilla> abierto = getVecinos(act, cerrado, destino,sinSalida);
+
+		ArrayList<Casilla> vecinos = getVecinos(act, cerrado, destino, sinSalida);
+		ArrayList<Casilla> abierto = getVecinos(act, cerrado, destino, sinSalida);
 
 		calcularPesoVecinos(destino, vecinos, abierto, act);
-		
-	
+
 		while (!cerrado.contains(destino)) {
 			try {
 				act = Collections.min(vecinos, (o1, o2) -> Double.compare(o1.getF(), o2.getF()));
-//				if(todosVecinos.contains(act)) {
-//					System.out.println("Pasa otra vez");
-//					act=todosVecinos.get(todosVecinos.indexOf(act));
-//					borrarDeCerradoPorVuelta(act,cerrado);
-//				}
-				System.out.println("ACT: " + act);
-				agregarMinimoCerrado(cerrado,act,abierto);
+				act = agregarMinimoCerrado(cerrado, act, abierto);
 				vecinos = getVecinos(act, cerrado, destino, sinSalida);
-				
+
 				ponerSinRepetir(abierto, vecinos);
 				calcularPesoVecinos(destino, vecinos, abierto, act);
-					
+
 			} catch (NoSuchElementException e) {
-				if(cerrado.isEmpty())
-					return cerrado;
-				sinSalida.add(cerrado.remove(cerrado.size()-1));
-				vecinos = getVecinos(cerrado.get(cerrado.size()-1), cerrado, destino, sinSalida);
+				if (cerrado.isEmpty())
+					return cerrado;// se quedo sin salida y no hay solucion
+				sinSalida.add(cerrado.remove(cerrado.size() - 1));
+				vecinos = getVecinos(cerrado.get(cerrado.size() - 1), cerrado, destino, sinSalida);
 				System.out.println("act: " + act);
-			}		
-			todosVecinos.addAll(vecinos);
-			
+			}
 		}
 
-		System.out.println();
 		ArrayList<Casilla> camino = new ArrayList<Casilla>();
-		int i = cerrado.size() - 1;
-		camino.add(cerrado.get(i));
-		while (!camino.contains(origen)) {
-			camino.add(cerrado.get(i).getpadre());
-			i--;
-		}
+		crearCamino(cerrado, camino);
 		return camino;
 	}
 
-
-	private void borrarDeCerradoPorVuelta(Casilla act, ArrayList<Casilla> cerrado) {
-		for (int i = cerrado.indexOf(act); i < cerrado.size(); i++) {
-			cerrado.remove(i);
+	private void crearCamino(ArrayList<Casilla> cerrado, ArrayList<Casilla> camino) {
+		Casilla aux = cerrado.get(cerrado.size() - 1);
+		camino.add(aux);
+		while (aux != null) {
+			aux = cerrado.get(cerrado.indexOf(aux)).getpadre();
+			camino.add(aux);
 		}
 	}
 
-	private void agregarMinimoCerrado(ArrayList<Casilla> cerrado, Casilla act, ArrayList<Casilla> abierto) {	
-		if(act.getF() > abierto.get(abierto.indexOf(act)).getF()) {
+	private boolean casillaValida(Casilla c) {
+		return c.getX() <= x && c.getX() >= 0 && c.getY() <= y && c.getY() >= 0;
+	}
+
+	private Casilla agregarMinimoCerrado(ArrayList<Casilla> cerrado, Casilla act, ArrayList<Casilla> abierto) {
+		if (act.getF() > abierto.get(abierto.indexOf(act)).getF()) {
 			act = abierto.get(abierto.indexOf(act));
 		}
-		
+
 		cerrado.add(act);
+		return act;
 	}
 
 	private void ponerSinRepetir(ArrayList<Casilla> abierto, ArrayList<Casilla> vecinos) {
@@ -116,7 +116,6 @@ public class Mapa {
 				abierto.add(vec);
 			else if (abierto.get(abierto.indexOf(vec)).getF() > vec.getF()) {
 				int pos = abierto.indexOf(vec);
-				System.out.println("REMPLAZOOO");
 				abierto.remove(pos);
 				abierto.add(pos, vec);
 			}
@@ -129,21 +128,8 @@ public class Mapa {
 
 		for (int i = 0; i < vecinos.size(); i++) {
 			Casilla sucesor = vecinos.get(i);
-//			Casilla sucesor = new Casilla(vecinos.get(i).getX(),vecinos.get(i).getY());
-//			Double distanciaActSucesor = getDistanciaEntreCasilas(sucesor, act);
-//			Double distanciaActDestino = getDistanciaEntreCasilas(sucesor, destino);
-//
-//			sucesor.setG(act.getG() + distanciaActSucesor);
-//			sucesor.setH(distanciaActDestino);
-//			sucesor.setpadre(act);
-
 			int indiceEnAbierto = abierto.indexOf(sucesor);
 			if (abierto.get(indiceEnAbierto).getF() > sucesor.getF()) {
-//				System.out.println("-------------------------------------");
-//				System.out.println("de: " + abierto.get(indiceEnAbierto) + " a: " + sucesor);
-//				System.out.println(" padre Viejo : " + abierto.get(indiceEnAbierto).getpadre() + " padre NUEVO : "
-//						+ sucesor.getpadre());
-//				System.out.println("-------------------------------------");
 				abierto.remove(indiceEnAbierto);
 				abierto.add(indiceEnAbierto, sucesor);
 			}
@@ -163,12 +149,13 @@ public class Mapa {
 		return a < b ? a : b;
 	}
 
-	private ArrayList<Casilla> getVecinos(Casilla c, ArrayList<Casilla> cerrado, Casilla destino, ArrayList<Casilla> sinSalida) {
+	private ArrayList<Casilla> getVecinos(Casilla c, ArrayList<Casilla> cerrado, Casilla destino,
+			ArrayList<Casilla> sinSalida) {
 		int inicioX = c.getX() - 1;
 		int inicioY = c.getY() - 1;
 		ArrayList<Casilla> rta = new ArrayList<Casilla>();
-		for (int i = inicioX; i < inicioX + 3 && i < terreno.length; i++) {
-			for (int j = inicioY; j < inicioY + 3 && j < terreno[0].length; j++) {
+		for (int i = inicioX; i < inicioX + 3 && i < this.x; i++) {
+			for (int j = inicioY; j < inicioY + 3 && j < this.y; j++) {
 				Casilla aux = new Casilla(i, j);
 				if (i >= 0 && j >= 0 && !esObstaculo(aux) && !cerrado.contains(aux) && !sinSalida.contains(aux)) {
 					Double distanciaActSucesor = getDistanciaEntreCasilas(aux, c);
@@ -186,16 +173,16 @@ public class Mapa {
 
 	public void printCamino(ArrayList<Casilla> camino) {
 		System.out.print("   ");
-		for (int i = 0; i < terreno[0].length; i++) {
+		for (int i = 0; i < this.y; i++) {
 			System.out.print(" " + i + " ");
 		}
 		System.out.println();
-		for (int i = 0; i < terreno.length; i++) {
-			if (i>9)
-				System.out.print( i + " ");
+		for (int i = 0; i < this.x; i++) {
+			if (i > 9)
+				System.out.print(i + " ");
 			else
 				System.out.print(" " + i + " ");
-			for (int j = 0; j < terreno[0].length; j++) {
+			for (int j = 0; j < this.y; j++) {
 				if (!esObstaculo(i, j)) {
 					Casilla aux = new Casilla(i, j);
 					if (camino.contains(aux))
